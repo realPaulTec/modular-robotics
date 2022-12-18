8bbed33d4c3fb39ed8dbe927da24f6651eda69e6
realPaulTec <p.loeffler@alweb.de>
2022-12-18 16:25:52 -0500

    Implementing .kv file

--- src/interface.py
+++ src/interface.py
@@ -2,29 +2,31 @@ import kivy
 import cv2
 
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.clock import Clock
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from kivy.lang.builder import Builder
 from kivy.graphics.texture import Texture
+from kivy.properties import ObjectProperty
 
 import trackingV5 as tv5
 
 trackingSystem = tv5.systemTrack(0)
 
-class MyApp(App):
+class MainGridLayout(Widget):
+    pass
+
+class UserInterface(App):
+    cvImageTexture = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.cvImage = Image()
-        print(self.cvImage.texture)
-
         Clock.schedule_interval(self.callback, 0.05)
 
     def build(self):
-        layout = BoxLayout()
-        layout.add_widget(self.cvImage)
-
-        return layout
+        # Building the MGL
+        return MainGridLayout()
 
     def convertCvTexture(self, frame):
         buffer = cv2.flip(frame, 0)
@@ -33,15 +35,14 @@ class MyApp(App):
         texture = Texture.create(size = (frame.shape[1], frame.shape[0]), colorfmt = 'bgr')
         texture.blit_buffer(buffer, colorfmt = 'bgr', bufferfmt = 'ubyte')
 
-        print(texture)
-
         return texture
 
     def callback(self, *largs):
+        # Reading the opencv frame
         components, frame, out = trackingSystem.mainCycle()
-        self.cvImage.texture = self.convertCvTexture(frame)
 
-        # trackingSystem.simpleUI(frame)
+        # Converting opencv frame to Kivy texture
+        self.cvImageTexture = self.convertCvTexture(frame)
 
 if __name__ == '__main__':
-    MyApp().run()
+    UserInterface().run()
